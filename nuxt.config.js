c2b19219c8d9d408569c37a84084f676c6c0b074
Naomi Mango <naomi.mango@gmail.com>
2022-02-22 14:07:11 +0100

    [MAJ] TUTO Nuxt JS - Ajout de Bootstrap

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,47 +1,52 @@
 export default {
-  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+    // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+    ssr: false,
 
-  // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+    // Target: https://go.nuxtjs.dev/config-target
+    target: "static",
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'interfaceCommande',
-    htmlAttrs: {
-      lang: 'en'
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: "interfaceCommande",
+        htmlAttrs: {
+            lang: "en",
+        },
+        meta: [
+            { charset: "utf-8" },
+            {
+                name: "viewport",
+                content: "width=device-width, initial-scale=1",
+            },
+            { hid: "description", name: "description", content: "" },
+            { name: "format-detection", content: "telephone=no" },
+        ],
+        link: [
+            { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+            {
+                rel: "stylesheet",
+                href: "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css",
+                integrity:
+                    "sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh",
+                crossorigin: "anonymous",
+            },
+        ],
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [],
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {},
+};
---
+++ pages/commandes.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container">
+        <h1 class="display-1">Commander</h1>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+</style>
