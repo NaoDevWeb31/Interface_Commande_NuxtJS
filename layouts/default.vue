a97ecf84cdb364bbd0af054836253d273f2c4155
Naomi Mango <naomi.mango@gmail.com>
2022-02-22 15:37:52 +0100

    [MAJ] TUTO Nuxt JS - Menu de navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,26 @@
 <template>
     <div>
-        <Nuxt />
+        <nav class="navbar navbar-dark bg-dark sticky-top">
+            <ul class="navbar-nav flex-row">
+                <li class="nav-item mr-3">
+                    <nuxt-link class="nav-link" to="/"> Accueil </nuxt-link>
+                </li>
+                <li class="nav-item">
+                    <nuxt-link class="nav-link" to="/commandes">
+                        Commandes
+                    </nuxt-link>
+                </li>
+            </ul>
+        </nav>
+        <div
+            class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0"
+        >
+            <link
+                href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css"
+                rel="stylesheet"
+            />
+            <Nuxt />
+        </div>
     </div>
 </template>
 
