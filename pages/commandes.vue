44f49d7b8f908b7fe81fb489253564e53fcf7a60
Naomi Mango <naomi.mango@gmail.com>
2022-02-22 16:22:39 +0100

    [MAJ] TUTO Nuxt JS - Produits dispo

--- pages/commandes.vue
+++ pages/commandes.vue
@@ -1,11 +1,20 @@
 <template>
     <div>
         <titre-page :titre="titre" />
-        <div class="container">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus,
-            alias aliquam repellat at obcaecati, ipsa suscipit dolore laudantium
-            delectus, necessitatibus accusamus similique fugit distinctio
-            adipisci rem ipsam quibusdam cum aliquid.
+        <div class="absolute top-1/4 left-0 w-screen">
+            <h2 class="mx-6 mb-6 h2">Produits disponibles</h2>
+            <div class="card-deck mx-6">
+                <div
+                    v-for="produit in produits"
+                    :key="produit.nom"
+                    class="card text-white bg-dark mb-3 w-auto"
+                >
+                    <div class="card-header">{{ produit.nom }}</div>
+                    <div class="card-body">
+                        <p class="card-text">{{ produit.prix }}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -20,6 +29,12 @@ export default {
     data() {
         return {
             titre: "Commander",
+            produits: [
+                { nom: "Pizza", prix: 3.0 },
+                { nom: "Frites", prix: 2.0 },
+                { nom: "Hamburger", prix: 2.5 },
+                { nom: "Cheeseburger", prix: 3.0 },
+            ],
         };
     },
 };
